c4e1103efe49c2768dcc705825b52fedc93dff03
Limahcode <abimbolaalimat12@gmail.com>
2022-10-13 15:03:20 -0700

    finished

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Main Function to write a quote
+ * Return: Return 1 if successful
+ */
+
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, q, 59);
+	return (1);
+}
