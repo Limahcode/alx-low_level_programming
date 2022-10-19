2bce4582d6055d1a4a6b3d28be0679a9f96155a3
Limahcode <abimbolaalimat12@gmail.com>
2022-10-18 20:27:57 -0700

    wonderful

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <main.h>
 /**
- *If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Write a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line
-*Return: always 0 (success)
-*/
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
+ *
+ * Return: Always (0).
+ */
 
-int main() 
+int main(void)
 {
+	int i, sum;
 
-  int sum = 0;
-
-	for (int i = 3; i < 1000; i++) 
+	sum = 0;
+	for (i = 0; i < 1024; i++)
 	{
-
-		if (i%3==0 or i%5==0)
-
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
+	printf("%d\n", sum);
 
-	cout << sum;
-
-	return 0;
+	return (0);
 }
