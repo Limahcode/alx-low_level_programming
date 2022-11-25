d429df1dc8f3894f65f6b5a0d8bdc44212f27615
Limahcode <abimbolaalimat12@gmail.com>
2022-11-24 23:33:30 -0800

    chibby

---
+++ 0x12-singly_linked_lists/0-main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+}
